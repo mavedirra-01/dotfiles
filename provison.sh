4abfbbc62791f8aea0e5bdc0fa0462955c6f48d6
mavedirra-01 <62550704+mavedirra-01@users.noreply.github.com>
2021-10-23 22:42:26 -0300

    Update provison.sh

--- provison.sh
+++ provison.sh
@@ -2,9 +2,8 @@
 DEBIAN_FRONTEND=noninteractive
 apt update && apt install -y neofetch wget kitty vim nano zsh openvpn tmux git
 runuser -l osi -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
-#'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
 chsh -s /usr/bin/zsh osi
-wget http://10.0.2.13/.zshrc /home/osi/.zshrc
+mv zshrc /home/osi/.zshrc
 git clone https://github.com/stevemcilwain/quiver.git /home/osi/.oh-my-zsh/custom/plugins/quiver &&  \
   git clone https://github.com/jhwohlgemuth/zsh-pentest.git /home/osi/.oh-my-zsh/custom/plugins/zsh-pentest && \
   git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/osi/.oh-my-zsh/custom/plugins/zsh-autosuggestions && \
@@ -12,5 +11,5 @@ git clone https://github.com/stevemcilwain/quiver.git /home/osi/.oh-my-zsh/custo
 #echo "xuser ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 runuser -l osi -c 'curl -sLf https://spacevim.org/install.sh | bash'
 #curl -sLf https://spacevim.org/install.sh | bash
-wget http://10.0.2.13/tmux.conf /home/osi/.tmux.conf
-wget http://10.0.2.13/init.toml /home/osi/.SpaceVim/init.toml
+mv tmux.conf /home/osi/.tmux.conf
+mv init.toml /home/osi/.SpaceVim/init.toml
